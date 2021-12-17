ef653c12a70e464c686df1b9edd26d58d331c150
tuyizerejean <tuyibobu@gmail.com>
2021-12-17 08:06:04 +0200

    Desinging Landing page with its Responsiveness

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const bugger = document.querySelector('.bugger')
+const navigation = document.querySelector('.navigation')
+bugger.addEventListener('click', function () {
+    navigation.classList.toggle('hide');
+})
+
